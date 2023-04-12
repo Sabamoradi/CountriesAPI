50b6038abfc52f5998c487a79b82e5a8a7cc30e5
sabamoradi <saba.moradi7719@gmail.com>
2023-04-12 16:38:06 +0430

    add input container

---
+++ src/components/global/Input/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-conatiner">
+    <template v-if="isShowAppendIcon">
+      <div class="icon"></div>
+    </template>
+    <input
+      :type="type"
+      :placeholder="placeText"
+      :value="value"
+      :name="name"
+      v-model="innerValue"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    isShowAppendIcon: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      default: "text",
+      type: String,
+    },
+    placeText: {
+      default: "",
+      type: String,
+    },
+    value: {
+      default: "",
+      type: String,
+    },
+    name: {
+      default: "",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      innerValue: "",
+    };
+  },
+};
+</script>
